99b9dd92ca72729dfffed623e5158d6a25b4650a
Zak Erving <zak@riptideio.com>
2019-07-19 12:51:04 +0200

    Grabbing all ng-binds

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-console.log("Hello, world!")
+const fs = require('fs')
+const path = require('path')
+
+const src = `./src`
+const ext = 'html'
+
+const parents = fs.readdirSync(src, 'utf8')
+const ngRegEx = new RegExp(/ng-bind="::i18n.[A-Za-z]+"/g)
+
+var files = walk(src)
+
+
+files.forEach(file => {
+  var data = fs.readFileSync(file, 'utf8')
+  var ngBinds = data.match(ngRegEx) || []
+  
+  ngBinds.forEach(ngBind => {
+    ngBind = ngBind.replace('ng-bind="::i18n.', '')
+    ngBind = ngBind.replace('"', '')
+    console.log(ngBind)
+  })
+
+})
+
+
+
+
+
+
+
+
+// UTILITIES ========================================
+// ==================================================
+function walk(dir) {
+  var results = []
+  var list = fs.readdirSync(dir)
+  list.forEach(function(file) {
+    file = dir + '/' + file
+    var stat = fs.statSync(file)
+    if (stat && stat.isDirectory()) { 
+      results = results.concat(walk(file))
+    } else {
+      let isExt = file.split('.').pop() == ext ? true : false
+      if(isExt) results.push(file)
+    }
+  })
+  return results
+}
